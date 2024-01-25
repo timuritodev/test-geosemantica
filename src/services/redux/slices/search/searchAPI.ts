a8b1cbb80b9685867d2100dc9118c17207762948
luciD <sosalnet@yandex.ru>
2024-01-25 18:43:33 +0300

    added REAME and some refactoring

--- src/services/redux/slices/search/searchAPI.ts
+++ src/services/redux/slices/search/searchAPI.ts
@@ -1,9 +1,9 @@
 const checkRes = (res: Response) => {
-	if (res.ok) {
-		return res;
-	} else {
-		return Promise.reject(res);
-	}
+  if (res.ok) {
+    return res;
+  } else {
+    return Promise.reject(res);
+  }
 };
 
 export const fetchData = (url: string, method: string) => {
@@ -14,13 +14,6 @@ export const fetchData = (url: string, method: string) => {
     },
   }).then((res) => checkRes(res));
 };
-
-// export const fetchSearch = (text: string): Promise<Response> => {
-//   return fetchData(
-//     `https://api.geoapify.com/v1/geocode/autocomplete?text=${text}&apiKey=2856b3c4c0964cb5b0f4bba275fe0eef`,
-//     "GET"
-//   ).then((res) => checkRes(res));
-// };
 
 export const fetchSearch = (text: string): Promise<Response> => {
   return fetchData(
--- src/types/Search.types.ts
+++ src/types/Search.types.ts
@@ -1,9 +1,5 @@
 import { IData } from "./Addres.types";
 
-export interface ISearch {
-  coordinates: string;
-}
-
 export interface ISearchState {
   status: "idle" | "success" | "loading" | "failed";
   error: string | undefined;
